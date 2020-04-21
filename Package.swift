24a0ae91355b4a61e8ae6010d681e299debc12fd
L1MeN9Yu <baal998.mophisto@gmail.com>
2020-04-21 15:38:31 +0800

    add multi products

--- Package.swift
+++ Package.swift
@@ -7,7 +7,10 @@ let package = Package(
         name: "Asura",
         products: [
             // Products define the executables and libraries produced by a package, and make them visible to other packages.
-            .library(name: "Asura", targets: ["Asura"]), ],
+            .library(name: "Asura", targets: ["Asura"]),
+            .library(name: "Asura.Dynamic", type: .dynamic, targets: ["Asura"]),
+            .library(name: "Asura.Static", type: .static, targets: ["Asura"]),
+        ],
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
